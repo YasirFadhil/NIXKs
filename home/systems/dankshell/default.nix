a01f6c28202fbb803d5b152701ac87e7ce942b9c
YasirFadhil <yasirfadhil46@gmail.com>
2026-02-01 18:55:16 +0700

    Create default.nix

---
+++ home/systems/dankshell/default.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.dms.homeModules.dank-material-shell
+  ];
+
+  programs.dank-material-shell = {
+    enable = true;
+    enableSystemMonitoring = true;
+    dgop.package = inputs.dgop.packages.${pkgs.system}.default;
+  };
+}
