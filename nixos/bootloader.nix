d209ef0700323d23eafa1379f8a00037d1460a34
YasirFadhil <yasirfadhil46@gmail.com>
2026-05-29 19:58:54 +0700

    change kernel and some params for my n4020

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -4,28 +4,26 @@
     loader = {
       systemd-boot = {
         enable = true;
-        # extraEntries = {
-        #   "arch.conf" = ''
-        #     title Arch Linux
-        #     linux /vmlinuz-linux
-        #     initrd /initramfs-linux.img
-        #     options root=UUID=b8deec5c-6601-4c78-b81f-ac2d7dd9b1be rw snd_intel_dspcfg.dsp_driver=3
-        #   '';
-        # };
       };
       efi.canTouchEfiVariables = true;
     };
-   # plymouth = {
-     # enable = false;
-     # theme = "mac-style";
-     # themePackages = [ pkgs.mac-style-plymouth ];
-   # };
-    # kernelPackages = pkgs.linuxPackages;
-    kernelPackages = pkgs.linuxPackages_latest;
+
+    # Force early loading of the Intel graphics driver to prevent switch-root freezes
+    initrd.kernelModules = [ "i915" ];
+
+    # Linux 6.12 is good, but if freezes persist, uncomment the LTS line below
+    kernelPackages = pkgs.linuxPackages_6_12;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages_zen;
-    # kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+    # kernelPackages = pkgs.linuxPackages_cachyos_lts;
+
     kernelParams = [
       "snd_intel_dspcfg.dsp_driver=3"
+      "i915.mitigations=off"
+      "intel_iommu=igfx_off"          # Fixed typo: changed hyphen to equals sign
+      "intel_idle.max_cstate=1"       # Prevents Gemini Lake low-power hardware freezes
+      "i915.enable_dc=0"              # Disables unstable GPU power-saving states
     ];
   };
 }
+
